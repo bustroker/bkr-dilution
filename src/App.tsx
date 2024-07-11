11f51e460299970ff2a7511aa55e8c0af1699aec
Luis Mesa <luismesagravedeperalta@gmail.com>
2024-07-11 11:57:25 +0200

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
-import Partner from "./Partner"
+
+interface PartnerProps {
+  id: number;
+  initialPercentage: number;
+  dilutedPercentage: number|undefined;
+  onRemove: (id: number) => void;
+  onChange: (id: number, value: number) => void;
+}
+
+const Partner: React.FC<PartnerProps> = ({ id, initialPercentage, dilutedPercentage, onRemove, onChange }) => {
+  return (
+    <div className="textbox-container">
+      <text className="partner-label">Initial shares percentage partner {id}</text>
+      <button className="minus-button" onClick={() => onRemove(id)}>
+        -
+      </button>
+      <input
+        type="number"
+        value={initialPercentage}
+        onChange={(e) => onChange(id, Number(e.target.value))}
+        className="number-input"
+      />
+      <text className="percentage-label">%</text>
+      
+      {dilutedPercentage != undefined &&
+      <div>
+          <text className="newpartner-label">{dilutedPercentage}</text>
+          <text className="newpercentage-label">%</text>
+      </div>
+      }
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   const [partners, setPartners] = useState<{ id: number; initialPercentage: number, dilutedPercentage: number|undefined }[]>([{id: 1, initialPercentage: 100, dilutedPercentage: undefined}]);
